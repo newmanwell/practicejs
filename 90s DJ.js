fa55b53ab629f56986f0bf25948760b4deaadbdb
Shaun Newman <shaunnewman@Shauns-Laptop.attlocal.net>
2024-08-03 07:08:58 -0400

    First commit

---
+++ 90s DJ.js
@@ -0,0 +1,17 @@
+let band = Math.ceil(Math.random() * 6);
+
+//console.log(band);
+
+if (band === 1) {
+    console.log('Pearl Jam');
+} else if (band === 2) {
+    console.log('Alice in Chains');
+} else if (band === 3) {
+    console.log('Soundgarden');
+} else if (band === 4) {
+    console.log('Silverchair');
+} else if (band === 5) {
+    console.log('Stone Temple Pilots');
+} else {
+    console.log('The Wallflowers')
+}
---
+++ coin flip.js
@@ -0,0 +1,9 @@
+let flip = Math.floor(Math.random() * 2);
+
+//console.log(flip);
+
+if (flip === 0) {
+    console.log('heads');
+} else {
+    console.log('tails');
+}
