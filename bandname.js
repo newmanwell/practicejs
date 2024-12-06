7d25a0ed6488ca374fd31059b11d8658cb375ae3
Shaun Newman <shaun.newman.p@gmail.com>
2024-12-06 16:18:23 -0500

    Added instruments to JS and HTML

--- bandname.js
+++ bandname.js
@@ -1,10 +1,11 @@
 const band = document.getElementById('bandBtn');
 const name = document.getElementById('bandName');
+const play = document.getElementById('instrument');
 
 band.onclick = function() {
     let first = Math.ceil(Math.random() * 7);
     let second = Math.ceil(Math.random() * 7);
-
+    let instrument = Math.ceil(Math.random() * 6);
     if (first === 1) {
         first = 'Inspired';
     } else if (first === 2) {
@@ -36,8 +37,32 @@ band.onclick = function() {
     } else if (second === 7) {
         second = 'Mastiffs'
     }
+
+    switch (instrument) {
+        case 1:
+            instrument = 'Lead Singer';
+            break;
+        case 2:
+            instrument = 'Lead Guitar';
+            break;
+        case 3:
+            instrument = 'Rhythm Guitar';
+            break;
+        case 4:
+            instrument = 'Bass Guitar';
+            break;
+        case 5:
+            instrument = 'Drums';
+            break;
+        case 6:
+            instrument = 'Tambourine'
+    }
+
+
     name.innerHTML = `${first} ${second}`;
+    play.innerHTML = `${instrument}`
     console.log(`${first} ${second}`);
+    console.log(`${instrument}`);
 }
 
 //generator();
